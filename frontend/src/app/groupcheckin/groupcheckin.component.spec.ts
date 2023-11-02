e01f3d52312aa2ba465c13b45303884e96b8a702
Sadiya Noor <sadiya@email.unc.edu>
2023-11-01 23:40:43 -0400

    Create groupcheckin component (#9)

---
+++ frontend/src/app/groupcheckin/groupcheckin.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GroupcheckinComponent } from './groupcheckin.component';
+
+describe('GroupcheckinComponent', () => {
+  let component: GroupcheckinComponent;
+  let fixture: ComponentFixture<GroupcheckinComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GroupcheckinComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GroupcheckinComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/groupcheckin/groupcheckin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-groupcheckin',
+  templateUrl: './groupcheckin.component.html',
+  styleUrls: ['./groupcheckin.component.css']
+})
+export class GroupcheckinComponent {}
