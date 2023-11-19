deec9ebb54300d7e01a36be1b83bd57f723948a2
fdahnoun <fdahnoun@email.unc.edu>
2023-11-18 20:50:56 -0500

    create new table for ambassador check in view

--- backend/models/coworking/reservation.py
+++ backend/models/coworking/reservation.py
@@ -58,3 +58,8 @@ class GroupReservation(BaseModel):
     users: list[str] = []
     when: datetime | None = None
     what: str = ""
+
+
+class AmbassadorReservation(BaseModel):
+    group_id: str = ""
+    status: bool
