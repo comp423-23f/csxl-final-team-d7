f8cdd624f5c9c16c8066ba68e4af480a04db9bad
Ali Jawad <37279871+AliJawad06@users.noreply.github.com>
2023-12-04 12:50:26 -0500

    Ajambassrez3 (#36)
    * dep
    
    * dep
    
    * remove checkout in out both tables a1
    
    * checkout button
    
    * fixed checkout rerende
    
    ---------
    
    Co-authored-by: Ferras Dahnoun <69730341+fdahnoun@users.noreply.github.com>

--- frontend/src/app/checkin-reservation/checkin-reservation.component.ts
+++ frontend/src/app/checkin-reservation/checkin-reservation.component.ts
@@ -4,16 +4,22 @@ import { Observable } from 'rxjs';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { GroupService } from '../group.service';
 import { AmbassadorGroupReservation } from '../coworking/coworking.models';
+import { ChangeDetectorRef } from '@angular/core';
+
 @Component({
   selector: 'app-display-group-id',
   templateUrl: './checkin-reservation.component.html',
   styleUrls: ['./checkin-reservation.component.css']
 })
 export class GroupListComponent implements OnInit {
-  groupIds: string[] = [];
-  isCheckInMode: boolean = true; // Track whether it's check-in or check-out
+  public notCheckedIn: AmbassadorGroupReservation[] = [];
+  public checkedIn: AmbassadorGroupReservation[] = [];
+  public isCheckInMode: boolean = true; // Track whether it's check-in or check-out
   public ambassGroups: AmbassadorGroupReservation[] = [];
-  constructor(private groupService: GroupService) {}
+  constructor(
+    private groupService: GroupService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   public static Route = {
     path: 'checkin-reservation',
@@ -23,8 +29,12 @@ export class GroupListComponent implements OnInit {
   ngOnInit(): void {
     this.groupService.getAmbassGroups().subscribe(
       (data: AmbassadorGroupReservation[]) => {
-        this.ambassGroups = data;
-        console.log(data + 'this is data');
+        data.forEach((item: AmbassadorGroupReservation) => {
+          item.status
+            ? this.checkedIn.push(item)
+            : this.notCheckedIn.push(item);
+          console.log(item);
+        });
       },
       (error) => {
         console.error('Error fetching ambassador groups:', error);
@@ -35,9 +45,15 @@ export class GroupListComponent implements OnInit {
   handleCheckIn(x: string) {
     this.groupService.checkInGroup(x).subscribe(
       (data: AmbassadorGroupReservation) => {
-        this.ambassGroups = [data]; // Wrap the single object in an array
-        console.log(data, 'this is data');
-        location.reload();
+        var foundObject: any = this.notCheckedIn.find(
+          (item) => item.group_id === x
+        );
+        foundObject = {...foundObject, status: true};
+        this.notCheckedIn = this.notCheckedIn.filter(
+          (item) => item.group_id !== x
+        );
+        // Wrap the single object in an 'array
+        this.checkedIn = [...this.checkedIn, foundObject];
       },
       (error) => {
         console.error('Error checking in the ambassador group:', error);
@@ -47,9 +63,10 @@ export class GroupListComponent implements OnInit {
   handleCheckOut(x: string) {
     this.groupService.checkOutGroup(x).subscribe(
       (data: AmbassadorGroupReservation) => {
-        this.ambassGroups = [data]; // Wrap the single object in an array
-        console.log(data, 'this is data');
-        location.reload();
+        const foundObject: any = this.checkedIn.find(
+          (item) => item.group_id === x
+        );
+        this.checkedIn = this.checkedIn.filter((item) => item.group_id !== x);
       },
       (error) => {
         console.error('Error checking in the ambassador group:', error);
